0b4bac50a1a255abb4e5fadfc7803d272a641c65
Mr. Spothawk <mr.spothawk@gmail.com>
2013-12-27 14:21:29 -0800

    colors working now. will start adding colored blocks to be exported.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -4,15 +4,27 @@ angular.module('caracolExtension', ['bookmarkService'])
     '$element', 
     'bookGetter', 
     function($scope, $element, bookmarkService){
+      
       $scope.bookmarks = [];
-      bookmarkService.getMarks(function(data){
-        
-          $scope.$apply(angular.forEach(data[0].children, function(val,key, col ){
-          $scope.bookmarks.push(val.children)
-        
+      
+      bookmarkService.getMarks(
+        function(data){
+          $scope.$apply(angular.forEach(data[0].children, 
+            function(val,key, col ){
+              $scope.bookmarks.push(val.children)
         }))
-      });
+      })
+
       
 
     }
-  ]);
+  ])
+  // .controller('ExportControl', [
+  //   function($scope) {
+  //     $scope.exports = {}
+
+  //   }
+  // ])
+
+
+
