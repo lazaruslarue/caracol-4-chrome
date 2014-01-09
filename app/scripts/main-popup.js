0f7e860198aa8116e6e7a42ade92ee8407b206ec
Mr. Spothawk <mr.spothawk@gmail.com>
2014-01-09 13:06:28 -0800

    minor styling corrections on the popup header

--- app/scripts/main-popup.js
+++ app/scripts/main-popup.js
@@ -30,52 +30,15 @@ angular.module('caracolExtension', [
         url: '/start/popup',
         views: {
           "head": {
-            template:'<h4 class="navbar">Your Caracol Clippings</h4>'
+            template:'<span style="position: fixed; top: 0; width:100%; background-color:white; border: 1px;"><h5>Your Caracol Clippings</h5></span>'
           },
           "focus": {
             templateUrl: 'views/popup.html',
             controller: 'fetchmyclippings'
-          }, 
-          // "right": {
-          //   templateUrl: 'views/export.html'
-            
-          // }
+          }
         }
       })
-      .state('caracol.processed', {
-        url: '/start/processed',
-        views: {
-          "head": {
-            templateUrl: 'views/header.html'
-          },
-          "focus": {
-            templateUrl: 'views/suggested.html',
-            controller: 'fetchmyrecommendations'
-          },
-          "right": {
-            templateUrl: 'views/userBookmarks.html', 
-            controller: 'fetchmyclippings'
-          },
-          "foot": {
-            templateUrl: 'views/processed.html',
-            controller: [ '$scope', '$state', 'servicefactory',
-              function(   $scope,  $state,   services){ 
-            }]
-          }
-        },
-         
-      })
-      // .state('caracol.caracolUserBar', {
-      //   url: '/caracolUserBar',
-      //   templateUrl: 'views/caracolUserBar.html'
-      // })
-      // .state('caracol.userBookmarks', {
-      //   url: '/userBookmarks',
-      //   templateUrl: 'views/userBookmarks.html'
-      // })
-      // .state('caracol.suggestedBookmarks', {
-      //   url: '/suggestedBookmarks',
-      //   templateUrl: 'views/suggestedBookmarks.html'
-      // })
-}]);
+    }
+  ]
+);
 
