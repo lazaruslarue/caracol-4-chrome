b86a130fe46d652d82fcf896f15a24a79e8bf2b9
Mr. Spothawk <mr.spothawk@gmail.com>
2014-01-08 17:12:38 -0800

    refactor export state to use controller

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -6,10 +6,32 @@ angular.module('caracolExtension.controllers',[])
       $scope.bookmarks = {};
     }
   ])
-  .controller('firstRun', [
-    function($scope) {
-      $scope.exports = {}
+  .controller('firstRun', [ '$scope', '$state', 'bookmarkUtils',
+    function(   $scope,  $state,   bookmarkService){ 
+      var traverseTreeWrapper = function(node){
+        var callback = function(node){
+          node['caracolSubmitStatus'] = 'alert-info';
+          $scope.bookmarks[node.id]= node;
+        };
+        bookmarkService.traverseTree(node, callback);
+      }
 
+      bookmarkService.getMarks( function(data){$scope.$apply(traverseTreeWrapper(data[0]))} )
+
+      $scope.toggleUrlSubmitStatus=function(id, obj) { // add this Url to the the JSON for export to Caracol server
+        bookmarkService.toggleShading(obj.caracolSubmitStatus, obj); 
+        if (  obj.caracolSubmitStatus === "alert-danger" ||
+              obj.caracolSubmitStatus === "alert-info" ) {
+          delete $scope.exports[id];
+        } else {
+          $scope.exports[id] = obj
+        }
+      }
+
+      $scope.submitUrlsWrapper = function(){
+        bookmarkService.submitUrls($scope.exports);
+        $state.go('caracol.processed')
+      }
     }
   ])
 
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -33,39 +33,9 @@ angular.module('caracolExtension', [
           "head": {
             templateUrl: 'views/header.html'
           },
-          "body": {
+          "focus": {
             templateUrl: 'views/export.html',
-            controller: [ '$scope', '$state', 'bookmarkUtils',
-              function(   $scope,  $state,   bookmarkService){ 
-               
-                
-                var traverseTreeWrapper = function(node){
-                  var callback = function(node){
-                    node['caracolSubmitStatus'] = 'alert-info';
-                    $scope.bookmarks[node.id]= node;
-                  };
-                  bookmarkService.traverseTree(node, callback);
-                }
-
-                bookmarkService.getMarks( function(data){$scope.$apply(traverseTreeWrapper(data[0]))} )
-
-                $scope.toggleUrlSubmitStatus=function(id, obj) { // add this Url to the the JSON for export to Caracol server
-                  bookmarkService.toggleShading(obj.caracolSubmitStatus, obj); 
-                  if (  obj.caracolSubmitStatus === "alert-danger" ||
-                        obj.caracolSubmitStatus === "alert-info" ) {
-                    delete $scope.exports[id];
-                  } else {
-                    $scope.exports[id] = obj
-                  }
-                }
-
-                $scope.submitUrlsWrapper = function(){
-                  bookmarkService.submitUrls($scope.exports);
-                  $state.go('caracol.processed')
-                }
-              }
-            ]
-            
+            controller: 'firstRun'
           }
         }
       })
@@ -75,14 +45,14 @@ angular.module('caracolExtension', [
           "head": {
             templateUrl: 'views/header.html'
           },
-          "body": {
+          "focus": {
             templateUrl: 'views/suggested.html'
-            
           },
           "foot": {
             templateUrl: 'views/processed.html',
             controller: [ '$scope', '$state', 'bookmarkUtils',
               function(   $scope,  $state,   bookmarkService){ 
+
             }]
           }
         },
